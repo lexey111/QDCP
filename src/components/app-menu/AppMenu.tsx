aab976f280659787fb24424679806180a6431767
Oleksii Koshkin <lexey111@gmail.com>
2024-06-08 20:53:09 +0200

    Add redirect

--- src/components/app-menu/AppMenu.tsx
+++ src/components/app-menu/AppMenu.tsx
@@ -1,7 +1,23 @@
 import './AppMenu.css'
-import {NavLink} from "react-router-dom"
+import {NavLink, redirect, useSearchParams} from "react-router-dom"
+import {useEffect} from "react"
+
+const baseName = import.meta.env.VITE_SITE_BASE
 
 export const AppMenu = () => {
+	const [searchParams] = useSearchParams();
+
+	useEffect(() => {
+		if (searchParams?.get('path')?.includes(baseName)) {
+			const redirectUrl = searchParams?.get('path')?.split('/').pop()
+
+			if (redirectUrl) {
+				console.log('redirect', baseName + '/' + redirectUrl)
+				redirect(baseName + '/' + redirectUrl);
+			}
+		}
+	}, []);
+
 	return (
 		<div className={'app-menu'}>
 			<NavLink to={'/home'}>Home</NavLink>
